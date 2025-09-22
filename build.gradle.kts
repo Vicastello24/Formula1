7919d8dd0d30bdddca9123d6e3fbc2ceb80bfc96
HERNÁNDEZ GONZÁLEZ <vichergon5@a1-04.infor.caminas>
2025-09-22 16:50:44 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "1.9.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(22)
+}
---
+++ src/main/kotlin/XML.kt
@@ -0,0 +1,56 @@
+import java.nio.file.Path
+import java.io.File
+// Anotaciones y clases de la librería Jackson para el mapeo a XML.
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+/*Representa la estructura de una única planta. La propiedad 'id_planta' será la
+etiqueta <id_planta>...</id_planta> (así todas) */
+//nombre del elemento raíz
+// Data class que representa el elemento raíz del XML.
+
+data class Pilotos(
+    @JacksonXmlElementWrapper(useWrapping = false) // No necesitamos la etiqueta <plantas> aquí
+    @JacksonXmlProperty(localName = "piloto")
+    val listaPilotos: List<Piloto> = emptyList()
+)
+
+
+fun main() {
+    val entradaXML = Path.of("Datos/pilotos_f1.xml")
+    val salidaXML = Path.of("Datos/DatosF1_2.xml")
+    val datos: List<Piloto>
+    datos = leerDatosInicialesXML(entradaXML)
+    for (dato in datos) {
+        println(" - ID: ${dato.ID}, Nombre: ${dato.Nombre}, Escudería: ${dato.Escudería}, Dorsal: ${dato.Dorsal}, Victorias: ${dato.Victorias}, Podios: ${dato.Podios}, Puntos Medios por Temporada: ${dato.PuntosMedios}")
+    }
+    escribirDatosXML(salidaXML, datos)
+}
+fun leerDatosInicialesXML(ruta: Path): List<Piloto> {
+//var plantas: List<Planta> =emptyList()
+    val fichero: File = ruta.toFile()
+// Deserializar el XML a objetos Kotlin
+    val xmlMapper = XmlMapper().registerKotlinModule()
+// 'readValue' convierte el contenido XML en una instancia de la clase 'Plantas'
+    val pilotosWrapper: Pilotos = xmlMapper.readValue(fichero)
+    return pilotosWrapper.listaPilotos
+}
+fun escribirDatosXML(ruta: Path,pilotos: List<Piloto>) {
+    try {
+        val fichero: File = ruta.toFile()
+// Creamos instancia de la clase 'Plantas' (raíz del XML).
+        val contenedorXml = Pilotos(pilotos)
+// Configuramos el 'XmlMapper' (motor de Jackson) para la conversión a XML.
+        val xmlMapper = XmlMapper().registerKotlinModule()
+// Convertimos 'contenedorXml' en un String con formato XML.
+// .writerWithDefaultPrettyPrinter() formatea con indentación y saltos de línea
+        val xmlString = xmlMapper.writerWithDefaultPrettyPrinter().writeValueAsString(contenedorXml)
+// escribir un String en un fichero con 'writeText'
+        fichero.writeText(xmlString)
+        println("\nInformación guardada en: $fichero")
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
+    }
+}
